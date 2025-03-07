58af7e1513b996d84de91d415bbdfd07836c4201
Abhishek <abhirsingh16@gmail.com>
2025-03-07 09:26:27 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT ='-e .'
+
+def get_requirements(file_path:str) ->List[str]:
+    '''Read the requirements file and return list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+    
+setup(
+    name='project2_google_apps',
+    version='0.1',
+    author='Abhishek Singh',
+    author_email='abhirsingh16@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirement.txt')
+)
